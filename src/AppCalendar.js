62f13c9ab15838d53d08e3224d8e41d642b5bacc
esmailisamane <s.esmaili30@gmail.com>
2022-03-23 10:48:56 +0100

    calanderCss

--- src/AppCalendar.js
+++ src/AppCalendar.js
@@ -7,7 +7,8 @@ import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import "./App.css";
+import "./AppCalendar.css"
+
 
 
 const locales = {
@@ -50,10 +51,11 @@ function AppCalendar() {
     }
 
     return (
-        <div className="App">
+        <div className="userCalendar ">
+            <div className="background">
             <h1>Calendar</h1>
             <h2>Add New Event</h2>
-            <div>
+            <div >
                 <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
                 <DatePicker placeholderText="Start Date" style={{ marginRight: "10px" }} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
                 <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
@@ -62,6 +64,7 @@ function AppCalendar() {
                 </button>
             </div>
             <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+        </div>
         </div>
     );
 }
